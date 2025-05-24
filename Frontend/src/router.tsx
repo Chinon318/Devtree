01294ecfb2e56a7d8e7ba012a771f454614bf66e
Chinon318 <giltryx618@gmail.com>
2025-05-24 10:23:44 -0600

    Modificaciones

--- Frontend/src/router.tsx
+++ Frontend/src/router.tsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LoginView from './views/LoginView'
 import RegisterView from './views/RegisterView'
 import AuthLayout from './layouts/AuthLayout'
+import AppLayout from './layouts/AppLayout'
+import LinkTreeView from './views/LinkTreeView'
+import ProfileView from './views/ProfileView'
 
 
 export default function router(){
@@ -9,9 +12,14 @@ export default function router(){
     return(
         <BrowserRouter>
             <Routes>
-                <Route element={<AuthLayout/>}>
-                    <Route path='/auth/login' element ={<LoginView/>}/>
-                    <Route path='/auth/register' element ={<RegisterView/>}/>
+                <Route path='/auth' element={<AuthLayout/>}>
+                    <Route path='login' element ={<LoginView/>}/>
+                    <Route path='register' element ={<RegisterView/>}/>
+                </Route>
+
+                <Route path='/admin' element={<AppLayout/>}>
+                    <Route index={true} element={<LinkTreeView/>}/>
+                    <Route path='profile' element={<ProfileView/>}/>
                 </Route>
             </Routes>
         </BrowserRouter>
